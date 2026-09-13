d5561cf065bb17507cfd4f3a77d62b9b50a98a60
jongsu <jongsu9702@gmail.com>
2026-09-13 15:34:21 -0400

    Complete Lab 1

--- lab1.py
+++ lab1.py
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 
+=======
+# What is your full name
+print("Jongsu Seo")
+# The area of a rectangle is length * width.
+# What is the area of a rectangle with a length of 10 and width of 7?
+print(10 * 7)
+# Fix the line below. Interpreters scan your entire file ahead-of-time to check for errors, unlike compilers.
+print(False)
+# Add one more color to the end of the list, after blue. (Don't forget the comma , and quotes "")
+print(["red", "orange", "blue", "white"])
+>>>>>>> f3fe42a (Complete Lab 1)
